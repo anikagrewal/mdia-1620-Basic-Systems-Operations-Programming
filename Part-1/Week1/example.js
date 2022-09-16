b49c2ab4e51523aa3df5a9b808ebe38de0bc6b8b
Anika Grewal <agrewal117@my.bcit.ca>
2022-09-16 12:15:27 -0700

    adding more code

--- Part-1/Week1/example.js
+++ Part-1/Week1/example.js
@@ -1,3 +1,55 @@
 // Declare a variable called car
 
 var car;
+
+// camelCase
+// variable called bugs collected and initalise it to the value 4 
+
+var bugsCollected = 4;
+
+// PascalCase
+// variable called bugs collected and initalise it to the value 4
+
+var BugsCollected = 4;
+
+// Snake_case
+// variable called bugs collected and initalise it to the value 4
+
+var Bugs_Collected = 4;
+
+// Initialise the variable electronics box to the word cpu 
+// Use camelCase
+
+var electronicsBox = "cpu";
+
+// Boolean
+// Initialise the variable called proceed to the value true
+
+var proceed = true;
+var i = 0;
+
+while(proceed) {
+    i++; // i = 1;
+    proceed = false;
+};
+
+console.log(i);
+
+// Data Type - Object
+/**
+ * 6-4-1 Create a variable called grannySmith and add the
+ * following values (use camelCase):
+ * colour fo the apple is green
+ * origin location is australia
+ * origin date is 1868
+ * Then console log out the colour from the object 
+ */
+
+// Create an object:
+var grannySmith = {
+    color: "green",
+    originLocation: "australia",
+    originDate: 1868
+};
+
+console.log(grannySmith);
